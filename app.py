5bfde63745fa8476d7efd798a0a500a748809a9f
L-Q-K <kykise@gmail.com>
2017-10-05 19:40:08 +0700

    Commit

--- app.py
+++ app.py
@@ -17,24 +17,6 @@ girl_type = GirlType(name= "Gái hư",
 
 app = Flask(__name__)
 
-girl_types = [
-            {
-                'name': "Gái tiểu thư",
-                'image': "https://via.placeholder.com/400x200",
-                'description': "Thường sang chảnh, thích nuông chiều, đòi hỏi thanh niên cần có chút 'vốn', thích đàn ông sạch sẽ, ăn mặc gọn gàng và có nhiều tài lẻ"
-            },
-            {
-                'name': "Gái ngoan",
-                'image': "https://via.placeholder.com/400x200",
-                'description': "ăn mặc giản dị, không đua đòi, chịu khó, tính cách cẩn thận,chăm chỉ, chịu khó, hay đọc sách"
-            },
-            {
-                'name': "Gái hư",
-                'image': "https://via.placeholder.com/400x200",
-                'description': "Hay đua đòi, ăn chơi, đi chơi qua đêm, bia, rượu,..."
-            }
-]
-
 @app.route('/')
 def index():
     return render_template('index.html', girl_types=GirlType.objects())
@@ -48,4 +30,5 @@ def about():
 def school():
     return redirect("http://techkids.vn")
 
-app.run(debug=True)
+if __name__ == '__main__':
+  app.run(debug=True)
